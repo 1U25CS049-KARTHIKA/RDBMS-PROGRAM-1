2b69bc8c44cb4f808104208a11d4941479fb1a84
1U25CS049-KARTHIKA <karthikaabirami75@gmail.com>
2026-08-21 11:20:28 +0530

    Update starter.sql

--- starter.sql
+++ starter.sql
@@ -1,1 +1,7 @@
-
+create database colldb;
+use colldb;
+create table department(DepartmentID integer(5)primary key,departmentname varchar(20),HODvarchar(12)NOT NULL);
+desc department;
+insert department value("10030","computer science","karthika");
+select* from department;
+desc department;
